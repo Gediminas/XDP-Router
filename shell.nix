c1b23b6be6a944dbd245c40de766334041fbd021
Gediminas Lūžys <gediminas.luzys@gmail.com>
2024-03-09 08:58:34 +0200

    Switch to static linking with MUSL

--- shell.nix
+++ shell.nix
@@ -15,21 +15,22 @@ mkShell {
   ];
 
   shellHook = ''
-    if ! cargo version 2> /dev/null; then
+    if ! cargo version 2> /dev/null || [ ! -f ".prepared_rustup" ]; then
       echo "Installing rustup"
       rustup default stable
+      rustup target add x86_64-unknown-linux-gnu
+      rustup target add x86_64-unknown-linux-musl
       rustup toolchain install nightly
-
       rustup component add rust-src
       rustup component add rust-analyzer
-      rustup component add cargo-deb
-
-      cargo install bpf-linker
-      # export PATH=$PATH:$HOME/.nix-profile/bin
+      touch .prepared_rustup
     fi
 
     echo ">>>>> $name"
     echo -n ">>>>> "; gcc   --version | head -n1 | awk '{print $1"  ", $3, $2}'
+    pushd router-xdp >/dev/null
+    echo -n ">>>>> "; cargo --version
+    popd >/dev/null
     echo -n ">>>>> "; cargo --version
   '';
 }
